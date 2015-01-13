e8002f7f3b510602021a53b8e9f1185ed86ac843
Nathan Files <nfiles2009@gmail.com>
2015-01-13 10:52:24 -0500

    remove threading

--- algorithms/implementation/sherlock_and_queries/sherlock_and_queries.py
+++ algorithms/implementation/sherlock_and_queries/sherlock_and_queries.py
@@ -1,7 +1,5 @@
 #! /bin/python
 
-import threading
-import time
 from math import pow
 
 def main():
@@ -10,42 +8,21 @@ def main():
     B  = map(int, raw_input().strip().split())
     C = map(int, raw_input().strip().split())
 
-    threads = []
     for i in xrange(0, M):
-        thread = DividesThread(A, B[i], C[i])
-        threads.append(thread)
-
-    for thread in threads:
-        thread.start()
-
-    for thread in threads:
-        thread.join(1)
+        for j in xrange(0, N):
+            if divides(j+1, B[i]):
+                A[j] *= C[i]
 
     mod = int(pow(10, 9) + 7)
     print " ".join(map(lambda num: str(num % mod), A))
 
-# ref: http://stackoverflow.com/a/24066054/2548291
-def timed_join_all(threads, timeout):
-    start = cur_time = time.time()
-    while cur_time < (start + timeout):
-        for thread in threads:
-            if not thread.is_alive():
-                thread.join()
-        time.sleep(1)
-        cur_time = time.time()
+def static_var(name, value):
+    def decorate(func):
+        setattr(func, name, value)
+        return func
+    return decorate
 
-class DividesThread(threading.Thread):
-    def __init__(self, A, b, c):
-        threading.Thread.__init__(self)
-        self.A = A
-        self.b = b
-        self.c = c
-
-    def run(self):
-        for j in xrange(0, len(self.A)):
-            if divides(j+1, self.b):
-                self.A[j] *= self.c
-
+@static_var("cache", dict())
 def divides(dividend, divisor):
     tmp = divides.cache
     if dividend not in tmp:
@@ -55,8 +32,7 @@ def divides(dividend, divisor):
     if divisor not in tmp:
         tmp[divisor] = (dividend % divisor == 0)
 
-    return divides.cache[dividend][divisor]
-divides.cache = dict()
+    return tmp[divisor]
 
 if __name__ == "__main__":
     main()
